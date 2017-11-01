df2b79664f229d22c9b9bcb40c1ad622a72d46d8
LarissaMorrell <larissa@larissamorrell.com>
2017-11-01 16:42:47 -0400

    added grid for company info page

--- src/components/company-info.js
+++ src/components/company-info.js
@@ -8,20 +8,23 @@ export default class CompanyInfo extends React.Component {
     let company = this.props.company;
 
     return (
-      <div>
-        <h1>{company.name}</h1>
-        <p>{company.streetAddress}<br/>
-            {company.city}, {company.state}
-        </p>
-        <div>
-          <Rating rating={company.rating} />
-          <p>{company.totalReviewCount} reviews</p>
+      <div className="box pure-g">
+        <div className="pure-u-3-5">
+          <h1>{company.name}</h1>
+          <p>{company.streetAddress}<br/>
+              {company.city}, {company.state}
+          </p>
+          <div>
+            <Rating rating={company.rating} />
+            <p>{company.totalReviewCount} reviews</p>
+          </div>
         </div>
-        <div>
-          <DrinkTypes types={company.types} />
-
-        </div>
+        <div className="pure-u-2-5">
         <img className="company" src={`/media/${company.image}`} alt="company"/>
+          <div>
+            <DrinkTypes types={company.types} />
+          </div>
+      </div>
         <p>Hours: </p>
           <ul>
             {hours(company.hours)}
