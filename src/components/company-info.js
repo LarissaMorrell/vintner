5c411786a4a6d11803d440d64d2778c1a805bf55
LarissaMorrell <larissa@larissamorrell.com>
2017-11-07 15:20:38 -0500

    Attempted 2 columns hours, changed back to table but code is commented below. Revisit

--- src/components/company-info.js
+++ src/components/company-info.js
@@ -12,15 +12,20 @@ export default class CompanyInfo extends React.Component {
         <div className="pure-g">
             <div className="pure-u-7-12">
               <h1>{company.name}</h1>
-              <p>{company.streetAddress}<br/>
-                  {company.city}, {company.state}
-              </p>
-              <DrinkTypes types={company.types} />
-              <div>
-                <Rating rating={company.rating} />
-                {company.totalReviewCount} reviews
+              <h3 className="biz-address">{company.streetAddress}, {company.city}, {company.state}
+              </h3>
+
+              <div className="pure-g">
+                <div className="pure-u-1-2">
+                  <Rating className="" rating={company.rating} />
+                  {company.totalReviewCount} reviews
+                </div>
+                <div className="pure-u-1-2">
+                  <DrinkTypes types={company.types} />
+                </div>
               </div>
-              <p>Hours:</p>
+
+              <h3>Hours:</h3>
               <table className="table-simple hours-table">
                 <tbody>
                   {genHours(company.hours)}
@@ -36,30 +41,49 @@ export default class CompanyInfo extends React.Component {
   }
 }
 
-function hours(hours){
-  return hours.map((hour, i) =>
-      <li key={i}>
-        {hour}
-      </li>
-  );
-}
 
-function generateHours(hours){
-  let dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
-  return hours.map((hour, i) => {
-    return(
-      <div className="pure-g">
-        <div className="pure-u-1-5">
-          {dayOfWeek[i]}
-        </div>
-        <div className="pure-u-4-5">
-          {hour}
-        </div>
-      </div>
-    )
-  })
-}
+// function generateHours(hours){
+//   let dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+//   let hourElements = '<div className="pure-g">';
+//   let leftCol = 0;
+//   let rightCol = 4;
+//
+//   for(let i = 0; i < 4; i++){
+//     hourElements +=
+//       <div className="pure-u-1-2">
+//         <span className="side-bar">{dayOfWeek[leftCol]}</span> {hours}
+//       </div>;
+//     leftCol++;
+//     rightCol++;
+//   }
+//   hourElements += '</div>';
+//
+//   return hourElements;
+// }
+// function generateHours(hours){
+//   let dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+//   let hourElements = <div className="pure-g">
+//     <div className="pure-u-1-2">;
+//
+//   hourElements += hours.map((hour, i) => {
+//     if(i % 3 == 0){
+//       hourElements += </div><div="pure-u-1-2">
+//     }
+//
+//     return(
+//       <div>
+//         <span className="side-bar">
+//           {dayOfWeek[i]}
+//         </span>
+//           {hour}
+//       </div>
+//     )
+//   })
+//
+//   hourElements += </div></div>;
+//   return hourElements;
+// }
 
 function genHours(hours){
   let dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
