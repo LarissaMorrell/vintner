be09bee392f3f133f9387eca1f1f293f2f1fa923
LarissaMorrell <larissa@larissamorrell.com>
2017-11-13 10:39:07 -0500

    fixed navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -19,42 +19,43 @@ export class Navbar extends React.Component {
         if(this.props.loggedIn){
           //if loggedIn render the profile link, logout button
           listItems = [
-            <div className="pure-u-1-5" key="1">
-              <Link to="/company">Find Booze!</Link>
-            </div>,
-            <div className="pure-u-1-5 nav-item" key="2">
-              <Link to="/dashboard">Profile</Link>
-            </div>,
+            <li className="pure-menu-item nav-item" key="1">
+              <Link to="/company" className="pure-menu-link nav-link">Find Booze!</Link>
+            </li>,
+            <li className="pure-menu-item nav-item" key="2">
+              <Link className="pure-menu-link nav-link"to="/dashboard">Profile</Link>
+            </li>,
             //TODO turn on button functionality for modal
-            <div className="pure-u-1-5" key="3">
-              <button onClick={() => this.logOut()}>Log out</button>
-            </div>
+            <li className="pure-menu-item" key="3">
+              <button className="pure-menu-link" onClick={() => this.logOut()}>Log out</button>
+            </li>
           ];
         }
         else {
           listItems = [
-            <div className="pure-u-1-5" key="1">
-              <Link to="/company">See Reviews</Link>
-            </div>,
-            <div className="pure-u-1-5" key="2">
-              <Link to="/register">Sign Up</Link>
-            </div>,
-            <div className="pure-u-1-5" key="3">
-              <button onClick={e => this.props.dispatch(openModal(<LoginForm />))}>Login</button>
-            </div>
+            <li className="pure-menu-item nav-item" key="1">
+              <Link className="pure-menu-link nav-link" to="/company">Find Booze!</Link>
+            </li>,
+            <li className="pure-menu-item nav-item" key="2">
+              <Link className="pure-menu-link nav-link" to="/register">Sign Up</Link>
+            </li>,
+            <li className="pure-menu-item" key="3">
+              <button className="pure-menu-link" onClick={e => this.props.dispatch(openModal(<LoginForm />))}>Login</button>
+            </li>
           ]
         }
 
         return (
-          <div className="navbar">
-            <div className="nav-content pure-g">
-              <div className="pure-u-2-5" id="navbar-logo">Vintner</div>
+          <div className="navbar pure-menu pure-menu-horizontal">
+            <div className="pure-menu-heading" id="nav-brand">Vintner</div>
+            <ul className="pure-menu-list">
               {listItems}
-            </div>
+            </ul>
           </div>
         );
     }
 }
+
 
 const mapStateToProps = state => ({
     loggedIn: state.auth.currentUser !== null
