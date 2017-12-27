0ecce80eb6c0965269c3c20705b43e8c3621371e
LarissaMorrell <larissa@larissamorrell.com>
2017-12-27 15:37:08 -0500

    cleanup

--- src/components/drink-result.js
+++ src/components/drink-result.js
@@ -9,7 +9,6 @@ export class DrinkResult extends React.Component {
 
   render() {
     let drink = this.props.drink;
-    // let x = [...new Set(drink.reviews.map(review => review.flavors))];
     return (
       <div className="box result-container">
           <img className="drink-result" src={`/media/${drink.type}.png`} alt="drink type"/>
@@ -37,33 +36,6 @@ export class DrinkResult extends React.Component {
         </div>
       </div>
     )
-      //TODO add the overall winery rating under h1
-      //TODO add the drink types
   }
 }
 export default connect()(DrinkResult);
-
-// <p>
-//   {`People thought this ${drink.type} tasted: ${drink.flavors.join(', ')}`}
-// </p>
-
-//{popularFlavors(drink.reviews)}
-// function popularFlavors(reviews){
-  //extract all the flavor arrays from each review
-  //put the flavors into one array
-  //sort the arrays
-  //take out repeat flavors
-
-//   let flavors = reviews.map(review => review.flavors);
-//   console.log("flavors", flavors);
-//   let sortedFlavors = ...flavors.slice().sort();
-//   console.log("sorted flavors", sortedFlavors);
-//   let popFlav = [];
-//
-//   for (var i = 0; i < sortedFlavors.length - 1; i++) {
-//       if (sortedFlavors[i + 1] === sortedFlavors[i]) {
-//           popFlav.push(sortedFlavors[i]);
-//       }
-//   }
-//   return sortedFlavors;
-// }
