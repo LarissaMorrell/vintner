bb6cfb6aa6805c2c04122320e8432a7e4a71788a
LarissaMorrell <larissa@larissamorrell.com>
2017-12-27 12:00:30 -0500

    add drink details

--- src/components/drink-result.js
+++ src/components/drink-result.js
@@ -14,9 +14,11 @@ export class DrinkResult extends React.Component {
       <div className="box result-container">
           <img className="drink-result" src={`/media/${drink.type}.png`} alt="drink type"/>
           <div className="pure-g drink-result">
-          <div className="pure-u-1-1 pure-u-md-3-5 result-title">
-            <Link to={`/drink/${drink.id}`}>{drink.name}</Link>
-            {/* TODO Add flavors, price, etc here */}
+          <div className="pure-u-1-1 pure-u-md-3-5">
+            <div className="result-title">
+              <Link to={`/drink/${drink.id}`}>{drink.name}</Link>
+            </div>
+            <p className="result-details">{drink.description}</p>
           </div>
           <div className="pure-u-1-1 pure-u-md-1-5">
             <Rating rating={drink.rating} />
