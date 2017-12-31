ee8e7ab498c90dd7129e2d15732dc5de6c9c7bfb
LarissaMorrell <larissa@larissamorrell.com>
2017-12-31 18:17:20 -0500

    added drink obj

--- src/__tests__/drink-result.js
+++ src/__tests__/drink-result.js
@@ -4,8 +4,29 @@ import {shallow, mount} from 'enzyme';
 import DrinkResult from '../components/drink-result';
 
 describe('<DrinkResult />', () => {
-    it('Renders without crashing', () => {
-        shallow(<DrinkResult key=1 drink= />);
-    });
+  let drink = {
+    id: "5a43df85e5662405fc2cf553",
+    name: "Cranberry Apple",
+    type: "wine",
+    description: "A New England tradition. Tart and flavorful, with berries and local Red Delicious apples.",
+    company: "5a21a31a1957ae0ffa7e2e02",
+    reviews: [
+      {
+        id: "5a452d5cd4cb490d85ed76cb",
+        comment: "This wine is fantastic with poultry!",
+        drink: "5a43df85e5662405fc2cf553",
+        flavors: ["Semi-Dry", "Fruity"],
+        price: 2,
+        purchased: true,
+        rating: 4,
+        title: "It's Delicious!",
+        user: "5a43caf791ed5f0277800ee9"
+      }
+    ]
+  }
+
+  it('Renders without crashing', () => {
+    shallow(<DrinkResult key={1} drink= />);
+  });
 
 });
