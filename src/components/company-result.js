9ef9e7cf4d570b0d2446348e650a8881453115be
LarissaMorrell <larissa@larissamorrell.com>
2018-01-25 17:19:40 -0500

    (fixed) spacing for ratings col in company-result

--- src/components/company-result.js
+++ src/components/company-result.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import Rating from './rating';
-import DrinkTypes from './drink-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "./rating";
+import DrinkTypes from "./drink-types";
 
 export function CompanyResult(props) {
-    const company = props.company;
-    return (
-      <div className="result-container box pure-g">
-        <div className="pure-u-md-6-24 pure-u-sm-8-24 pure-u-1-1 biz-img-container">
-          <Link to={`/company/${company.id}`}>
-            <img className="company-result pure-img" src={company.imageUrl} alt="company" />
-          </Link>
-        </div>
-        <div className="pure-u-md-12-24 pure-u-sm-16-24 pure-u-1-1">
-          <div className="result-title">
-            <Link to={`/company/${company.id}`}>{company.name}</Link>
-          </div>
-          <p className="result-details">
-            {company.streetAddress}<br/>
-            {company.city}, {company.state}
-          </p>
-
-        </div>
-        <div className="pure-u-md-6-24 pure-u-sm-1-1">
-          <Rating rating={company.rating} />
-          <div className="review-count">
-            {genDrinksAvailableString(company.drinks.length)}
-          </div>
-          <DrinkTypes types={company.types} />
-        </div>
+  const company = props.company;
+  return (
+    <div className="result-container box pure-g">
+      <div className="pure-u-md-6-24 pure-u-sm-8-24 pure-u-1-1 biz-img-container">
+        <Link to={`/company/${company.id}`}>
+          <img
+            className="company-result pure-img"
+            src={company.imageUrl}
+            alt="company"
+          />
+        </Link>
       </div>
-    );
+      <div className="pure-u-md-12-24 pure-u-sm-16-24 pure-u-1-1">
+        <div className="result-title">
+          <Link to={`/company/${company.id}`}>{company.name}</Link>
+        </div>
+        <p className="result-details">
+          {company.streetAddress}
+          <br />
+          {company.city}, {company.state}
+        </p>
+      </div>
+      <div className="companies-review-count-container pure-u-md-6-24 pure-u-sm-1-1">
+        <Rating rating={company.rating} />
+        <div className="review-count">
+          {genDrinksAvailableString(company.drinks.length)}
+        </div>
+        <DrinkTypes types={company.types} />
+      </div>
+    </div>
+  );
 }
 
-function genDrinksAvailableString(count){
-  if(count === 1){
+function genDrinksAvailableString(count) {
+  if (count === 1) {
     return `${count} drink available`;
   }
   return `${count} drinks available`;
