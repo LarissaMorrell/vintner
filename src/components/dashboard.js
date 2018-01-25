7279e45e47277894b306efd54154e9bc574af93a
LarissaMorrell <larissa@larissamorrell.com>
2018-01-25 10:49:55 -0500

    (fixed) dashboard displaying for 1 or more reviews (added) demo link in login

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,86 +1,87 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {Redirect} from 'react-router-dom';
-import {fetchUser} from '../actions/users';
-import DrinkReview from './drink-review';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { fetchUser } from "../actions/users";
+import DrinkReview from "./drink-review";
 // import ReactLoading from 'react-loading';
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        if (!this.props.loggedIn) {
-            return;
-        }
-        this.props.dispatch(fetchUser());
+  componentDidMount() {
+    if (!this.props.loggedIn) {
+      return;
     }
+    this.props.dispatch(fetchUser());
+  }
 
-    render() {
-      // Dashboard is only visible to logged in users
-      if (!this.props.loggedIn) {
-          return <Redirect to="/" />;
-      }
-      let user = this.props.user;
+  render() {
+    // Dashboard is only visible to logged in users
+    if (!this.props.loggedIn) {
+      return <Redirect to="/" />;
+    }
+    let user = this.props.user;
+    console.log(user);
+    //TODO add spinner so undefined user does not display
+    // console.log(Object.getOwnPropertyDescriptor(user, 'username'));
+    // if(!(Object.getOwnPropertyDescriptor(user, 'username'))){
+    // if(!(user.username === undefined)){
+    //   console.log(user.username);
+    //   return(
+    //     <ReactLoading id="loading" className="center-horizontal" type="spin" color="#491722"/>
+    //   );
+    // }
 
-      //TODO add spinner so undefined user does not display
-      // console.log(Object.getOwnPropertyDescriptor(user, 'username'));
-      // if(!(Object.getOwnPropertyDescriptor(user, 'username'))){
-      // if(!(user.username === undefined)){
-      //   console.log(user.username);
-      //   return(
-      //     <ReactLoading id="loading" className="center-horizontal" type="spin" color="#491722"/>
-      //   );
-      // }
-
-
-      let reviews = this.props.user.reviews;
-      let drinkReviews;
-
-      if(reviews.length > 1){
-        drinkReviews = reviews.map((review, i) =>
-          <div className="box result-container" key={i}>
-            <div className="pure-g">
-              <div className="pure-u-sm-18-24  pure-u-1-1 sm-screen-center-horizontal">
-                <h1>{review.drink.name}</h1>
-              </div>
-              <div className="pure-u-sm-6-24 pure-u-1-1">
-                <Link to={`/drink/${review.drink._id}`}>
-                  <button className="visit-drink-button">
-                    Visit Drink
-                  </button>
-                </Link>
-              </div>
-              <div className="pure-u-1-1">
-                <DrinkReview review={review} type={review.drink.type} drinkId={review.drink._id} user={this.props.user}/>
-              </div>
+    let reviews = user.reviews;
+    let drinkReviews;
+    if (reviews.length > 0) {
+      drinkReviews = reviews.map((review, i) => (
+        <div className="box result-container" key={i}>
+          <div className="pure-g">
+            <div className="pure-u-sm-18-24  pure-u-1-1 sm-screen-center-horizontal">
+              <h1>{review.drink.name}</h1>
+            </div>
+            <div className="pure-u-sm-6-24 pure-u-1-1">
+              <Link to={`/drink/${review.drink._id}`}>
+                <button className="visit-drink-button">Visit Drink</button>
+              </Link>
+            </div>
+            <div className="pure-u-1-1">
+              <DrinkReview
+                review={review}
+                type={review.drink.type}
+                drinkId={review.drink._id}
+                user={this.props.user}
+              />
             </div>
           </div>
-        );
-      } else {
-        drinkReviews = <div className="result-container">You have not written any reviews yet.</div>
-      }
-      return (
-          <div className="content-container">
-            <div className="user-info-container">
-              <img src={`/media/avatars/${user.avatar}.png`} alt="avatar" />
-              <h1>
-                  {`${user.firstName} ${user.lastName}`}
-              </h1>
-              <div>
-                  {user.username}
-              </div>
-            </div>
-            {drinkReviews}
-          </div>
-        );
+        </div>
+      ));
+    } else {
+      drinkReviews = (
+        <div className="result-container">
+          You have not written any reviews yet.
+        </div>
+      );
     }
+    return (
+      <div className="content-container">
+        <div className="user-info-container">
+          <img src={`/media/avatars/${user.avatar}.png`} alt="avatar" />
+          <h1>{`${user.firstName} ${user.lastName}`}</h1>
+          <div>{user.username}</div>
+        </div>
+        {drinkReviews}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    const {currentUser} = state.auth;
-    return {
-        loggedIn: currentUser !== null,
-        user: state.protectedData.data
-    };
+  const { currentUser } = state.auth;
+  return {
+    loggedIn: currentUser !== null,
+    user: state.protectedData.data
+  };
 };
 
 export default connect(mapStateToProps)(Dashboard);
--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,65 +1,72 @@
-import React from 'react';
-import {Field, reduxForm, focus} from 'redux-form';
-import {Link} from 'react-router-dom';
-import Input from './input';
-import {login} from '../actions/auth';
-import {closeModal} from '../actions/modal';
-import DemoButton from './demo-button';
-import {required, nonEmpty} from '../validators';
+import React from "react";
+import { Field, reduxForm, focus } from "redux-form";
+import { Link } from "react-router-dom";
+import Input from "./input";
+import { login } from "../actions/auth";
+import { closeModal } from "../actions/modal";
+import DemoButton from "./demo-button";
+import { required, nonEmpty } from "../validators";
 
 export class LoginForm extends React.Component {
-    onSubmit(values) {
-        return this.props.dispatch(login(values.username, values.password));
-    }
+  onSubmit(values) {
+    return this.props.dispatch(login(values.username, values.password));
+  }
 
-    render() {
-        let error;
-        if (this.props.error) {
-            error = (
-                <div className="form-error" aria-live="polite">
-                    {this.props.error}
-                </div>
-            );
-        }
-        return (
-          <div>
-            <h2 className="form-title">
-              Sign in to your account
-            </h2>
-            <form
-                className="login-form"
-                onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
-                {error}
-                <Field
-                    component={Input}
-                    type="text"
-                    name="username"
-                    id="username"
-                    placeholder="Username"
-                    validate={[required, nonEmpty]}
-                />
-                <Field
-                    component={Input}
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="Password"
-                    validate={[required, nonEmpty]}
-                />
-                <div className="button-container">
-                  <button className="login-form" disabled={this.props.pristine || this.props.submitting}>
-                      Log in
-                  </button>
-                  <DemoButton classes="login-form" />
-                  <Link to="/register" onClick={() => this.props.dispatch(closeModal())}>Register</Link>
-                </div>
-            </form>
-          </div>
-        );
+  render() {
+    let error;
+    if (this.props.error) {
+      error = (
+        <div className="form-error" aria-live="polite">
+          {this.props.error}
+        </div>
+      );
     }
+    return (
+      <div>
+        <h2 className="form-title">Sign in to your account</h2>
+        <form
+          className="login-form"
+          onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+        >
+          {error}
+          <Field
+            component={Input}
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+            validate={[required, nonEmpty]}
+          />
+          <Field
+            component={Input}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            validate={[required, nonEmpty]}
+          />
+          <div className="button-container">
+            <button
+              className="login-form"
+              disabled={this.props.pristine || this.props.submitting}
+            >
+              Log in
+            </button>
+            {/*<DemoButton classes="login-form" />*/}
+            <Link
+              to="/register"
+              onClick={() => this.props.dispatch(closeModal())}
+            >
+              Register
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default reduxForm({
-    form: 'login',
-    onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
+  form: "login",
+  onSubmitFail: (errors, dispatch) => dispatch(focus("login", "username"))
 })(LoginForm);
