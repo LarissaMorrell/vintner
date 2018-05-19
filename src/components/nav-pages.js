f0748073ddd3530c7a83385a23d277edc7e79d0a
LarissaMorrell <larissa@larissamorrell.com>
2018-05-19 08:30:29 -0400

    cleanup

--- src/components/nav-pages.js
+++ src/components/nav-pages.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NavPages(props) {
-
-    return (
-      <div>
-        <Link className="pure-button page-title" to={props.route}>
-            <i className="fa fa-arrow-left" aria-hidden="true"></i>
-        </Link>
-        <h2 className="page-title">
-            {props.title}
-        </h2>
-        <hr/>
-      </div>
-    );
+  return (
+    <div>
+      <Link className="pure-button page-title" to={props.route}>
+        <i className="fa fa-arrow-left" aria-hidden="true" />
+      </Link>
+      <h2 className="page-title">{props.title}</h2>
+      <hr />
+    </div>
+  );
 }
